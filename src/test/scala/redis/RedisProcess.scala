b1337a366e3d3e8aaefa18d9c093603cc8725dbc
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2019-02-25 00:26:09 +0700

    remove null

--- src/test/scala/redis/RedisProcess.scala
+++ src/test/scala/redis/RedisProcess.scala
@@ -10,31 +10,36 @@ import scala.sys.process.{Process, ProcessLogger}
 import scala.util.control.NonFatal
 
 class RedisProcess(val port: Int) {
-  var server: Process         = null
-  val cmd                     = s"${redisServerCmd} --port $port ${redisServerLogLevel}"
-  protected val log           = Logger(getClass)
-  protected val processLogger = ProcessLogger(line => log.debug(line), line => log.error(line))
+  protected var maybeServer: Option[Process] = None
+  protected val cmd                               = s"${redisServerCmd} --port $port ${redisServerLogLevel}"
+  protected val log                     = Logger(getClass)
+  protected val processLogger           = ProcessLogger(line => log.debug(line), line => log.error(line))
 
   def start(): Unit = {
     log.debug(s"starting $this")
-    if (server == null)
-      server = Process(cmd).run(processLogger)
+    maybeServer match {
+      case None    => maybeServer = Some(Process(cmd).run(processLogger))
+      case Some(_) => log.warn(s"$this already started")
+    }
   }
 
   def stop(): Unit = {
     log.debug(s"stopping $this")
-    if (server != null) {
-      try {
-        val out = new Socket(redisHost, port).getOutputStream
-        out.write("SHUTDOWN NOSAVE\n".getBytes)
-        out.flush()
-        out.close()
-      } catch {
-        case NonFatal(e) => log.error(s"couldn't stop $this", e)
-      } finally {
-        server.destroy()
-        server = null
-      }
+    maybeServer match {
+      case Some(s) =>
+        try {
+          val out = new Socket(redisHost, port).getOutputStream
+          out.write("SHUTDOWN NOSAVE\n".getBytes)
+          out.flush()
+          out.close()
+        } catch {
+          case NonFatal(e) => log.error(s"couldn't stop $this", e)
+        } finally {
+          s.destroy()
+          maybeServer = None
+        }
+      case None =>
+        log.warn(s"$this already stopped")
     }
   }
 
@@ -56,9 +61,9 @@ class SentinelProcess(masterName: String, masterPort: Int, port: Int) extends Re
     sentinelConfFile.path
   }
 
-  override val cmd = s"${redisServerCmd} $sentinelConfPath --port $port --sentinel $redisServerLogLevel"
+  override protected val cmd = s"${redisServerCmd} $sentinelConfPath --port $port --sentinel $redisServerLogLevel"
 }
 
 class SlaveProcess(masterPort: Int, port: Int) extends RedisProcess(port) {
-  override val cmd = s"$redisServerCmd --port $port --slaveof $redisHost $masterPort $redisServerLogLevel"
+  override protected val cmd = s"$redisServerCmd --port $port --slaveof $redisHost $masterPort $redisServerLogLevel"
 }
