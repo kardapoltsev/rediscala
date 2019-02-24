492d520f8eb2b4c37fc075e37da70fe8fce3f4e0
Alexey Kardapoltsev <alexey.kardapoltsev@gmail.com>
2019-02-24 11:27:50 +0700

    custom time factor for travis

--- src/test/scala/redis/SentinelMonitoredRedisClientMasterSlavesSpec.scala
+++ src/test/scala/redis/SentinelMonitoredRedisClientMasterSlavesSpec.scala
@@ -2,46 +2,6 @@ package redis
 
 import redis.RedisServerHelper.redisHost
 
-class SentinelMutablePoolSpec extends RedisSentinelClients("SentinelMutablePoolSpec") {
-
-  var redisPool: RedisClientMutablePool = null
-
-  override def setup(): Unit = {
-    super.setup()
-    redisPool = RedisClientMutablePool(Seq(RedisServer(redisHost, slavePort1)), masterName)
-  }
-
-  "mutable pool" should {
-    "add remove" in {
-      eventually {
-        redisPool.redisConnectionPool.size shouldBe 1
-      }
-
-      redisPool.addServer(RedisServer(redisHost, slavePort2))
-      redisPool.addServer(RedisServer(redisHost, slavePort2))
-      eventually {
-        redisPool.redisConnectionPool.size shouldBe 2
-      }
-
-      val key = "keyPoolDb0"
-      redisClient.set(key, "hello").futureValue
-
-      redisPool.get[String](key).futureValue shouldBe Some("hello")
-      redisPool.get[String](key).futureValue shouldBe Some("hello")
-
-      redisPool.removeServer(RedisServer(redisHost, slavePort2))
-
-      eventually {
-        redisPool.redisConnectionPool.size shouldBe 1
-      }
-
-      redisPool.get[String](key).futureValue shouldBe Some("hello")
-      redisPool.get[String](key).futureValue shouldBe Some("hello")
-    }
-  }
-
-}
-
 class SentinelMonitoredRedisClientMasterSlavesSpec
     extends RedisSentinelClients("SentinelMonitoredRedisClientMasterSlavesSpec") {
 
